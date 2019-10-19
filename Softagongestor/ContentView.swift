50a34d24da9074b95266b63ee0f3b7575fd574db
Hermes Alves Dias Souza <hermes@softagon.com.br>
2019-10-18 21:22:26 -0300

    Initial Commit

---
+++ Softagongestor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Softagongestor
+//
+//  Created by Hermes Alves on 18/10/19.
+//  Copyright © 2019 Softagon Inovação e Gestão. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
