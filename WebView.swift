cbf2d15762422f595acf5671fc6fe384b65cc5eb
Hermes Alves Dias Souza <hermes@softagon.com.br>
2019-10-21 18:41:04 -0300

    Iniciando o empacotamento

---
+++ WebView.swift
@@ -0,0 +1,21 @@
+//
+//  WebView.swift
+//  Softagongestor
+//
+//  Created by Hermes Alves on 18/10/19.
+//  Copyright © 2019 Softagon Inovação e Gestão. All rights reserved.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView()
+    }
+}
